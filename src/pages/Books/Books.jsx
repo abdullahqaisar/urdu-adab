f6f417789610016432c27f2df7b6cf1efe102572
abdullahqaisar <abdullahqaisarr@gmail.com>
2022-07-26 22:32:53 +0500

    added card in book section

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import TopNav from "../../components/TopNav";
 import Footer from "../../components/Footer";
@@ -8,7 +9,13 @@ function Books() {
   return (
     <Container fluid className="p-0">
       <TopNav />
-      <div>Books</div>
+      <Card className="card">
+        <Card.Img variant="top" className="cardImage" src={props.imageSrc} />
+        <Card.Body>
+          <Card.Title className="title">{props.title}</Card.Title>
+          <Card.Text className="text">{props.writer}</Card.Text>
+        </Card.Body>
+      </Card>
       <Footer />
     </Container>
   );
